77026e0f6c964125cf8b2e28db202c70df89a443
code-naz <mhnasrulloh.edu@gmail.com>
2026-04-19 16:06:28 +0700

    new update

--- api/index.php
+++ api/index.php
@@ -1,10 +1,2 @@
 <?php
-// api/index.php
-
-// 1. Paksa tampilkan error PHP mentah (karena Laravel-mu belum bangun)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// 2. Panggil bootstrap Laravel
 require __DIR__ . '/../public/index.php';
